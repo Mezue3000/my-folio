daae2be6287fa8ca3af89188703d00f194e9af58
mezue30000 <mezue3000>
2023-12-09 23:05:34 +0100

    imported carousel/progress bar

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { Carousel, ProgressBar } from 'react-bootstrap';
+import image1 from '../assets/Medias/img1.jpg';
+import image2 from '../assets/Medias/img2.webp';
+import image3 from '../assets/Medias/success.jpg';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section id='home' className='homy pb-5'>
+      <Carousel data-bs-theme="dark">
+          <Carousel.Item>
+            <img
+              className="d-block w-100 imagey"
+              src= {image1}
+              alt="First slide"
+            />
+              
+            <Carousel.Caption>
+              <p className='para1 text-white fw-bolder animate__backInDown'>Mezue Obiora Is A Fullstack Software Developer.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100 imagey"
+              src={image2}
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <p className='para1 text-primary fw-bolder animate__animated animate__backInDown'>Mezue Obiora Is A Fullstack Software Developer.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100 imagey"
+              src={image3}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <p className='para1 text-dark fw-bolder animate__animated animate__backInDown'>
+                Mezue Obiora Is A Fullstack Software Developer.
+              </p>
+            </Carousel.Caption>
+
+          </Carousel.Item>
+        </Carousel>
+
+        <div>
+          <h3 className='fs-2 ms-5 fw-bold text-info mt-1'>Cloud Computing</h3>
+          <ProgressBar className='w-75 ms-5' animated now={55} />;
+          <h3 className='fs-2 ms-5 fw-bold text-white'>React-Redux</h3>
+          <ProgressBar className='w-75 ms-5 mb-2' animated now={75} />
+        </div>
+    </section>
   )
 }
-
 export default Home
